edf7b356723476a7cff2c8d71fa0399aaaed58ed
Cattoen Axel <axel14022001@gmail.com>
2020-11-30 19:32:39 +0100

    add comment

--- makeVid.py
+++ makeVid.py
@@ -1,12 +1,15 @@
 import cv2
 import os
+
+# takes all transformed frames and assembles them
+
+
 cap=cv2.VideoCapture('/home/debian/testMosa/vid/Rick.mp4')
 fps=cap.get(cv2.CAP_PROP_FPS)
 path_img = '/home/debian/testMosa/dataPix2/'
 print('Création de la vidéo')
 liste=sorted(os.listdir(path_img))
 frame=cv2.imread(path_img+liste[0])
-# print(liste)
 h,l,c=frame.shape
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('/home/debian/testMosa/output40.avi',fourcc, fps, (l,h))
